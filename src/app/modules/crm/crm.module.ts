420a3fff8f20b23a107ab7a4f8299327871a1fa7
Jamel Yassin <jamelyassin84@gmail.com>
2022-01-22 15:35:51 +0400

    changes

--- src/app/modules/crm/crm.module.ts
+++ src/app/modules/crm/crm.module.ts
@@ -1,15 +1,15 @@
+import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { IconsModule } from './../../icons/icons.module'
 import { ToolbarsComponent } from './../../shared/toolbars/toolbars.component'
 import { CrmToolbarComponent } from './../../components/toolbars/crm-toolbar/crm-toolbar.component'
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AdminUserListComponent } from './admin-user/admin-user-list/admin-user-list.component'
 import { AdminUserDetailsComponent } from './admin-user/admin-user-details/admin-user-details.component'
 import { AppUserListComponent } from './app-user/app-user-list/app-user-list.component'
 import { AppUserDetailsComponent } from './app-user/app-user-details/app-user-details.component'
 import { PremiumUserListComponent } from './premium-user/premium-user-list/premium-user-list.component'
-
+import { BackToolbarComponent } from './../../components/toolbars/back-toolbar/back-toolbar.component'
 @NgModule({
 	declarations: [
 		AdminUserListComponent,
@@ -19,8 +19,18 @@ import { PremiumUserListComponent } from './premium-user/premium-user-list/premi
 		PremiumUserListComponent,
 		CrmToolbarComponent,
 		ToolbarsComponent,
+		BackToolbarComponent,
 	],
 	imports: [CommonModule, IconsModule, RouterModule],
-	exports: [CrmToolbarComponent, ToolbarsComponent],
+	exports: [
+		AdminUserListComponent,
+		AdminUserDetailsComponent,
+		AppUserListComponent,
+		AppUserDetailsComponent,
+		PremiumUserListComponent,
+		CrmToolbarComponent,
+		ToolbarsComponent,
+		BackToolbarComponent,
+	],
 })
 export class CRMModule {}
